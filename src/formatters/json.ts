2cf19a1ed8b40a9a4cdeeea4aac308719d95372b
Ульян Мудраков <umudrakov@MacBook-Pro-4.local>
2026-03-09 21:37:00 +0300

    refactor: remove old flat src/ files after DDD Onion migration

--- src/formatters/json.ts
+++
@@ -1,7 +0,0 @@
-import { Article, IFormatter } from '../types.js';
-
-export class JsonFormatter implements IFormatter {
-  format(articles: Article[]): string {
-    return JSON.stringify(articles, null, 2);
-  }
-}
--- src/formatters/registry.ts
+++
@@ -1,9 +0,0 @@
-import { IFormatter, OutputFormat } from '../types.js';
-
-export class FormatterRegistry {
-  constructor(private readonly formatters: Record<OutputFormat, IFormatter>) {}
-
-  get(format: OutputFormat): IFormatter {
-    return this.formatters[format];
-  }
-}
